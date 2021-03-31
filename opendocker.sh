fc9e94f3f9ea2c5914ade9c69bd77aef5fe6b5b1
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2021-04-01 02:27:42 +0900

    Change GUI settings

--- opendocker.sh
+++ opendocker.sh
@@ -3,7 +3,7 @@
 if [ $# -eq 0 ]; then
   echo 'opendocker.sh requires at least 1 argument.'
   echo ''
-  echo 'Usage: ./opendocker.sh IMAGE_NAME'
+  echo 'Usage: ./opendocker.sh <IMAGE_NAME>'
   exit 1
 fi
 
@@ -12,8 +12,8 @@ docker run -it --rm \
   --privileged \
   --gpus all \
   -e DISPLAY=$DISPLAY \
-  -v $HOME/.Xauthority:/root/.Xauthority \
-  -v /dev:/dev \
-  -v `pwd`/workspace:/workspace/ \
+  -v $HOME/X11-unix:/tmp/X11-unix \
+  --mount type=bind,src=/dev,dst=/dev \
+  --mount type=bind,src=$(pwd)/workspace,dst=/workspace/ \
   $1 \
   bash
